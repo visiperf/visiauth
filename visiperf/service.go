0b81e3ff5b9fe059ebbdfbf8164cd92fd8b0b645
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2020-07-06 15:11:58 +0200

    implement visiperf auth service

---
+++ visiperf/service.go
@@ -0,0 +1,32 @@
+package visiperf
+
+import (
+	"fmt"
+
+	"github.com/visiperf/visiauth"
+)
+
+type authService struct {
+	secret string
+}
+
+func NewAuthService(secret string) visiauth.AuthService {
+	return &authService{secret: secret}
+}
+
+func (as *authService) DecodeAccessToken(token string) (*visiauth.Jwt, error) {
+	jwt, err := newJwtFromToken(token)
+	if err != nil {
+		return nil, fmt.Errorf("token to jwt conversion error: %w", err)
+	}
+
+	if err := jwt.isValid(as.secret); err != nil {
+		return nil, fmt.Errorf("jwt validation error: %w", err)
+	}
+
+	if err := jwt.isExpired(); err != nil {
+		return nil, fmt.Errorf("jwt expiration error: %w", err)
+	}
+
+	return jwt.toVisiauthJwt(), nil
+}
