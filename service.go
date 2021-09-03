c7450c40e5af9d34075833859424063a00f0263c
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2021-09-03 18:12:26 +0200

    feat: implement validate method in auth0 service

--- service.go
+++ service.go
@@ -1,22 +1,54 @@
 package visiauth
 
+import "net/http"
+
+const defaultAuth0Domain = "dev-visiperf.eu.auth0.com"
+
 type Service interface {
 	Validate(accessToken string) error
 	User(accessToken string) User
 }
 
 type Auth0Service struct {
-	domain string
+	tokenParser TokenParser
 }
 
-func NewAuth0Service(domain string) *Auth0Service {
-	return &Auth0Service{domain}
+func NewAuth0Service(options ...Auth0ServiceOption) *Auth0Service {
+	opts := newDefaultAuth0ServiceOptions()
+	for _, opt := range options {
+		opt(opts)
+	}
+
+	return &Auth0Service{
+		tokenParser: NewJwtTokenParser(
+			NewAuth0PEMCertificateFetcher(opts.domain, &http.Client{}),
+		),
+	}
 }
 
 func (s *Auth0Service) Validate(accessToken string) error {
-	panic("not implemented") // TODO: Implement
+	_, err := s.tokenParser.ParseToken(accessToken)
+	return err
 }
 
 func (s *Auth0Service) User(accessToken string) User {
 	panic("not implemented") // TODO: Implement
+}
+
+type Auth0ServiceOptions struct {
+	domain string
+}
+
+func newDefaultAuth0ServiceOptions() *Auth0ServiceOptions {
+	return &Auth0ServiceOptions{
+		domain: defaultAuth0Domain,
+	}
+}
+
+type Auth0ServiceOption func(*Auth0ServiceOptions)
+
+func WithDomain(domain string) Auth0ServiceOption {
+	return func(opts *Auth0ServiceOptions) {
+		opts.domain = domain
+	}
 }
