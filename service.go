607c74248df556c780d23e3200e59e8b0135db09
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2021-09-03 11:37:25 +0200

    feat: create unimplemented auth0 service

--- service.go
+++ service.go
@@ -4,3 +4,19 @@ type Service interface {
 	Validate(accessToken string) error
 	User(accessToken string) User
 }
+
+type Auth0Service struct {
+	domain string
+}
+
+func NewAuth0Service(domain string) *Auth0Service {
+	return &Auth0Service{domain}
+}
+
+func (s *Auth0Service) Validate(accessToken string) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (s *Auth0Service) User(accessToken string) User {
+	panic("not implemented") // TODO: Implement
+}
