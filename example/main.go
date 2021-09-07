1089972515aa45b7badfb5d60449c3c1001859f0
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2021-09-07 16:26:27 +0200

    chore: update example

--- example/main.go
+++ example/main.go
@@ -8,5 +8,21 @@ import (
 )
 
 func main() {
-	log.Println(visiauth.NewAuth0Service().Validate(os.Getenv("VISIAUTH_AUTH0_ACCESS_TOKEN")))
+	accessToken := os.Getenv("VISIAUTH_AUTH0_ACCESS_TOKEN")
+	service := visiauth.NewAuth0Service()
+
+	if err := service.Validate(accessToken); err != nil {
+		log.Fatal(err)
+	}
+
+	user := service.User(accessToken)
+
+	log.Println(user.Id())
+	log.Println(user.Permissions())
+	log.Println(user.Roles())
+
+	for _, id := range user.OrganizationIds() {
+		log.Println(id)
+		log.Println(user.OrganizationRoles(id))
+	}
 }
