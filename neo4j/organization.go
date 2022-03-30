124610654e8cc8c0b3ff4b5cce24b3b5b90d2b70
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2022-03-30 18:09:14 +0200

    feat(neo4j): add some user roles

--- neo4j/organization.go
+++ neo4j/organization.go
@@ -11,11 +11,11 @@ func FetchOrganizationsByUser(ctx context.Context, userId string) (map[string]st
 	defer session.Close()
 
 	res, err := session.Run(`
-		match (u:User {uuid: $userId})-[ruo:WORKS_AT]->(o:Organization)
+		match (u:User {uuid: $userId})-[ruo:WORKS_AT|BUY_FOR|MANAGE|OWN]->(o:Organization)
 		return o.uuid as organization_uuid, type(ruo) as role
 		union all
-		match (u:User {uuid: $userId})-[ruh:WORKS_AT]->(h:Organization)-[rhn:HEAD_OF]->(n:Network)<-[ron:IN]-(o:Organization)
-		return o.uuid as organization_uuid, type(ruh) as role
+		match (u:User {uuid: $userId})-[ruh:WORKS_AT|BUY_FOR|MANAGE|OWN]->(h:Organization)-[rhn:HEAD_OF]->(n:Network)<-[ron:IN]-(o:Organization)
+		return o.uuid as organization_uuid, 'WORKS_AT' as role
 	`, map[string]interface{}{
 		"userId": userId,
 	})
@@ -26,7 +26,7 @@ func FetchOrganizationsByUser(ctx context.Context, userId string) (map[string]st
 	os := make(map[string]string)
 	for res.Next() {
 		values := res.Record().Values
-		os[values[0].(string)] = values[1].(string)
+		os[values[0].(string)] = mRoles[values[1].(string)]
 	}
 
 	if err := res.Err(); err != nil {
---
+++ neo4j/role.go
@@ -0,0 +1,9 @@
+package neo4j
+
+// TODO: use visiauth roles const
+var mRoles = map[string]string{
+	"WORKS_AT": "standard",
+	"BUY_FOR":  "buyer",
+	"MANAGE":   "manager",
+	"OWN":      "owner",
+}
