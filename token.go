f41deb70a0956be64d2800f8cede2ea90ec294c2
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2022-04-01 13:00:58 +0200

    feat(jwt): create token type

--- token.go
+++ token.go
@@ -2,9 +2,51 @@ package visiauth
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/golang-jwt/jwt"
 )
+
+type Token struct {
+	*jwt.Token
+}
+
+func NewToken(token *jwt.Token) *Token {
+	return &Token{token}
+}
+
+func (t Token) Header() map[string]interface{} {
+	return t.Token.Header
+}
+
+func (t Token) Kid() string {
+	return t.Header()["kid"].(string)
+}
+
+func (t Token) Claims() map[string]interface{} {
+	return t.Token.Claims.(jwt.MapClaims)
+}
+
+func (t Token) UserID() string {
+	return t.customKey("user_id")
+}
+
+func (t Token) Iss() string {
+	return t.Claims()["iss"].(string)
+}
+
+func (t Token) Scopes() []string {
+	return strings.Split(t.scope(), " ")
+}
+
+func (t Token) scope() string {
+	return t.Claims()["scope"].(string)
+}
+
+func (t Token) customKey(key string) string {
+	return fmt.Sprintf("https://%s%s", t.Iss(), key)
+}
 
 type TokenParser struct {
 	certificateFetcher *CertificateFetcher
