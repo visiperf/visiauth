6e55cf09a18cb32e0e4754c070f9b31f78cc1932
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2022-04-20 17:36:30 +0200

    test(token): add unit tests for retrieve token from pubsub message attribute

--- token_test.go
+++ token_test.go
@@ -1,7 +1,11 @@
 package visiauth
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"testing"
 
@@ -67,4 +71,62 @@ func TestRetrieveTokenFromRequest(t *testing.T) {
 			assert.Equal(t, test.err, err)
 		})
 	}
+}
+
+func TestRetrieveTokenFromPubSubMessageAttribute(t *testing.T) {
+	tests := []struct {
+		name  string
+		req   *http.Request
+		token string
+		err   error
+	}{{
+		name: "no body",
+		req: &http.Request{
+			Body: readCloser{},
+		},
+		err: &errReadCloser{},
+	}, {
+		name: "json invalid",
+		req: &http.Request{
+			Body: ioutil.NopCloser(bytes.NewBufferString(`{"message": {"attributes": "invalid value type"}}`)),
+		},
+		err: &json.UnmarshalTypeError{},
+	}, {
+		name: "no authorization key",
+		req: &http.Request{
+			Body: ioutil.NopCloser(bytes.NewBufferString(`{"message": {"attributes": {"key": "value"}}}`)),
+		},
+		err: ErrMissingAuthorization,
+	}, {
+		name: "ok",
+		req: &http.Request{
+			Body: ioutil.NopCloser(bytes.NewBufferString(fmt.Sprintf(`{"message": {"attributes": {"%s": "%sabc.def.ghi"}}}`, AuthorizationKey, authorizationPrefix))),
+		},
+		token: "abc.def.ghi",
+	}}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			token, err := RetrieveTokenFromPubSubMessageAttribute(test.req)
+
+			assert.Equal(t, test.token, token)
+			assert.IsType(t, test.err, err)
+		})
+	}
+}
+
+type errReadCloser struct{}
+
+func (e errReadCloser) Error() string {
+	return "read closer error"
+}
+
+type readCloser struct{}
+
+func (rc readCloser) Read(p []byte) (n int, err error) {
+	return 0, &errReadCloser{}
+}
+
+func (rc readCloser) Close() error {
+	return &errReadCloser{}
 }
